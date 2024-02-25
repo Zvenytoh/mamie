7915bf16c4e0c8cea7b4b8014682e1160c94c2cf
Mehdi <mehdiennajehfr@gmail.com>
2024-02-25 17:12:29 +0100

    fin du projet

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\AjoutCafeType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Cafe;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class BaseController extends AbstractController
 {
@@ -12,6 +17,27 @@ class BaseController extends AbstractController
     public function index(): Response
     {
         return $this->render('base/index.html.twig', [
+        ]);
+    }
+    
+    #[Route('/ajout-cafe', name: 'app_ajout_cafe')]
+    public function ajoutCafe(Request $request, EntityManagerInterface $em): Response
+    {
+        $cafe = new Cafe();
+        $form = $this->createForm(AjoutCafeType::class, $cafe);
+        
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if ($form->isSubmitted()&&$form->isValid()){
+                $em->persist($cafe);
+                $em->flush();
+                $this->addFlash('notice','Message envoyé');
+                return $this->redirectToRoute('app_ajout_cafe');
+            }
+            }
+           
+        return $this->render('base/ajout-cafe.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
